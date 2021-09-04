a24845f0d40590e711a122ba9df403d8b9470af6
boom210232 <chonchanok.che@ku.th>
2021-09-04 16:44:47 +0700

    Incomplete

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -13,14 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 
 from django.contrib import admin
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
-    path('', include('polls.urls')),
+    path('', views.index, name="main_index"),
     path('admin/', admin.site.urls),
 ]
