7c72bcf996d1f616f11922a01991dd518cc05c97
boom210232 <chonchanok.che@ku.th>
2021-09-04 17:35:30 +0700

    clean all not-used comment

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,11 +2,11 @@
 
 from django.http import HttpResponse, HttpResponseRedirect
 
-from django.template import loader
+# from django.template import loader
 
 from django.shortcuts import get_object_or_404, render
 
-from django.http import Http404
+# from django.http import Http404
 
 from django.urls import reverse
 
@@ -15,38 +15,6 @@ from .models import Choice, Question
 from django.views import generic
 
 from django.utils import timezone
-
-
-# def index(request):
-#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#     output = ', '.join([q.question_text for q in latest_question_list])
-#     return HttpResponse(output)
-
-# def index(request):
-#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#     template = loader.get_template('polls/index.html')
-#     context = {
-#         'latest_question_list': latest_question_list,
-#     }
-#     return HttpResponse(template.render(context, request))
-
-# def index(request):
-#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#     context = {'latest_question_list': latest_question_list}
-#     return render(request, 'polls/index.html', context)
-#
-#
-# # def detail(request, question_id):
-# #     try:
-# #         question = Question.objects.get(pk=question_id)
-# #     except Question.DoesNotExist:
-# #         raise Http404("Question does not exist")
-# #     return render(request, 'polls/detail.html', {'question': question})
-#
-#
-# def results(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
