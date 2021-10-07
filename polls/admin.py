f60f6d030155e9607bedb7dbf1f57494e6766bb5
boom210232 <chonchanok.che@ku.th>
2021-10-07 22:48:52 +0700

    Make all file in polls directory in flake8 style

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,4 @@
-from django.contrib import admin
-
+"""File for administrator panel."""
 # Register your models here.
 
 from django.contrib import admin
@@ -8,19 +7,25 @@ from .models import Choice, Question
 
 
 class ChoiceInline(admin.TabularInline):
+    """Display choice in admin panel."""
+
     model = Choice
     extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
+    """Class for make field set of polls with display and search."""
+
     fieldsets = [
         (None, {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date', 'end_date']}),
     ]
     inlines = [ChoiceInline]
-    list_display = ('question_text', 'pub_date', 'was_published_recently', 'end_date')
+    list_display = ('question_text', 'pub_date',
+                    'was_published_recently', 'end_date')
     list_filter = ['pub_date']
     search_fields = ['question_text']
+
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
--- polls/apps.py
+++ polls/apps.py
@@ -1,6 +1,10 @@
+"""Polls config for KU-polls."""
+
 from django.apps import AppConfig
 
 
 class PollsConfig(AppConfig):
+    """Polls config for KU-polls."""
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'polls'
