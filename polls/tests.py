6cf34e08e1241b208aa58ec85393d7505af589fc
boom210232 <chonchanok.che@ku.th>
2021-10-10 14:18:42 +0700

    Update docstring in tests.py and rename config.cfg to setup.cfg

--- polls/tests.py
+++ polls/tests.py
@@ -18,46 +18,26 @@ class QuestionModelTests(TestCase):
     """Class purpose is make sure that question will appear correctly."""
 
     def test_was_published_recently_with_future_question(self):
-        """
-        was_published_recently().
-
-        returns: False for questions whose pub_date
-        is in the future.
-        """
+        """Test that the future question is not published before publication time."""
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
 
     def test_was_published_recently_with_old_question(self):
-        """
-        was_published_recently().
-
-        returns: False for questions whose pub_date
-        is older than 1 day.
-        """
+        """Test that function will not publish old question."""
         time = timezone.now() - datetime.timedelta(days=1, seconds=1)
         old_question = Question(pub_date=time)
         self.assertIs(old_question.was_published_recently(), False)
 
     def test_was_published_recently_with_recent_question(self):
-        """
-        was_published_recently().
-
-        returns: True for questions whose pub_date
-        is within the last day.
-        """
+        """Test that the question is published recently correctly."""
         time = timezone.now() - \
             datetime.timedelta(hours=23, minutes=59, seconds=59)
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
 
     def test_can_vote_before_date(self):
-        """
-        Test can_vote function that it can vote before time or not.
-
-        return:
-            assertFalse because it can't vote before schedule.
-        """
+        """Test can_vote function that it can vote before time or not."""
         test_time = timezone.now() + \
             datetime.timedelta(hours=23, minutes=59, seconds=59)
         publish_question = Question(pub_date=test_time)
@@ -69,8 +49,8 @@ class QuestionModelTests(TestCase):
 
         Make sure that it show after the time that announced or not.
 
-        return:
-            assertTrue because it must show after the date
+        Assertion:
+            assertTrue when the poll is published after the publication date.
         """
         test_time = timezone.now() - \
             datetime.timedelta(hours=23, minutes=59, seconds=59)
