ec4219fb3438122d93e83e1d23036fe1130111b7
boom210232 <chonchanok.che@ku.th>
2021-10-31 15:45:07 +0700

    Add initation for seperate testcases

---
+++ polls/tests/test_detail_view.py
@@ -0,0 +1,52 @@
+# Create your tests here.
+
+import datetime
+
+from django.test import TestCase
+from django.utils import timezone
+from django.urls import reverse
+
+from polls.models import Question
+
+
+def create_question(question_text, days):
+    """
+    Create a question with the given `question_text`.
+
+    Published the given number of `days` offset to now
+     (negative for questions published in the past, positive for questions
+     that have yet to be published).
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+class QuestionDetailViewTests(TestCase):
+    """Objective for test question is question can vote in properly time."""
+
+    def test_future_question(self):
+        """
+        The detail view of a question with a pub_date in the future.
+
+        The result will redirect back to index with error word.
+
+        """
+        future_question = create_question(question_text='Future question.',
+                                          days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertRedirects(response, reverse('polls:index'), status_code=302,
+                             target_status_code=200, msg_prefix='',
+                             fetch_redirect_response=True)
+
+    def test_past_question(self):
+        """
+        This function detail view of a question with a pub_date in the past.
+
+        Make it displays the question's text.
+        """
+        past_question = create_question(question_text='Past Question.',
+                                        days=-5)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
